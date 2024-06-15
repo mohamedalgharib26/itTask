f6e127010aef652a420360434c2f9abe7f33c97f
mohamedalgharib26 <mohamedalgharib26@gmail.com>
2024-06-15 06:24:50 +0300

    initial Commit install packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexTitleSubtitle,
+  ApexXAxis,
+  NgApexchartsModule,
+} from 'ng-apexcharts';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgApexchartsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  series: ApexAxisChartSeries = [
+    {
+      name: 'Series 1',
+      data: [30, 40, 35, 50, 49, 60, 70, 91],
+    },
+  ];
+  chart: ApexChart = {
+    type: 'line',
+    height: 350,
+  };
+  xaxis: ApexXAxis = {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+  };
+  ChartTitle: ApexTitleSubtitle = {
+    text: 'Basic Line Chart',
+  };
   title = 'itTask';
 }
